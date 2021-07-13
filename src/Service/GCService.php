35da1cf852b2f945841e3787842747f63722721b
wwsh <tommix3@gmail.com>
2021-07-13 19:57:30 +0200

    - bug fix

--- src/Service/GCService.php
+++ src/Service/GCService.php
@@ -15,7 +15,7 @@ class GCService
     public function execute(): void
     {
         // remove rows older than 6 months
-        $moment = new \Datetime('-1 month');
+        $moment = new \Datetime('-6 month');
 
         $this->songDataRepository->deleteOlder($moment);
         $this->shoutDataRepository->deleteOlder($moment);
